a34ee9b63c33753215707a276d8e6aff42b17d86
Justin Cooper <jcooper@chicousd.org>
2019-05-15 15:17:37 -0700

    Set-PSCred reaplced with More flexible version

--- lib/Set-PSCred.ps1
+++ lib/Set-PSCred.ps1
@@ -1,13 +1,27 @@
-function Set-PSCred {
- [CmdletBinding()]
+<#
+This command can be dot sourced and used in scripts or it can be referenced directly
+#>
+[CmdletBinding()]
 param (
  # Username for credential object
- [Parameter(Position=0,Mandatory=$true)]
+ [Parameter(Position = 0, Mandatory = $false)]
  [string]$Username,
  # password string for credential object
- [Parameter(Position=1,Mandatory=$true)]
+ [Parameter(Position = 1, Mandatory = $false)]
  [string]$Password
 )
-$securePw = ConvertTo-SecureString $Password -AsPlainText -Force
-New-Object System.Management.Automation.PSCredential ($Username,$securePw )
+function Set-PSCred {
+ [CmdletBinding()]
+ param (
+  # Username for credential object
+  [Parameter(Position = 0, Mandatory = $true)]
+  [string]$Username,
+  # password string for credential object
+  [Parameter(Position = 1, Mandatory = $true)]
+  [string]$Password
+ )
+ $securePw = ConvertTo-SecureString $Password -AsPlainText -Force
+ New-Object System.Management.Automation.PSCredential ($Username, $securePw )
 }
+
+if ($Username -and $Password) { Set-PSCred $Username $Password }
